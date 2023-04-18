d46d9ee03b140bf059740ad6d1b8a816278ee8b4
jmrona <jm_rona@hotmail.com>
2023-04-18 18:26:33 +0100

    Adding redux-offline

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import { offline } from "@redux-offline/redux-offline";
+import offlineConfig from "@redux-offline/redux-offline/lib/defaults";
+
 import { rootReducer } from "./reducers/rootReducer";
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => {
-    return [...getDefaultMiddleware(), logger];
+    return [...getDefaultMiddleware(), logger, offline(offlineConfig)];
   },
 });
